d3d4edbd0f396b5e37a8e78ea80b315abe37cc0b
Georges-Xin Wen <gwen@f2r3s16.clusters.42paris.fr>
2026-02-12 12:27:56 +0100

    reorganization

---
+++ includes/minishell.h
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: storck <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/11 10:49:54 by storck            #+#    #+#             */
+/*   Updated: 2026/02/11 14:06:07 by storck           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# define _POSIX_C_SOURCE 200112L
+# define _XOPEN_SOURCE 600
+
+# include <curses.h>
+# include <dirent.h>
+# include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/ioctl.h>
+# include <sys/resource.h>
+# include <sys/stat.h>
+# include <sys/time.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <termios.h>
+# include <term.h>
+# include <unistd.h>
+
+# include "includes/libft/libft.h"
+
+typedef struct s_node
+{
+	int				type;
+	char			*value;
+	char			*arg;
+	struct s_node	*next;
+	struct s_node	*previous;
+}	t_node;
+
+/* parsing/parsing.c */
+char	*parsing(char *cmd);
+
+/* parsing/parsing-nod.c */
+t_node  *create_node(char *str, int len, t_node *list);
+char	**gen_arg_list(char **arg_list, int count_total, char *str);
+
+/* parsing/parisng-utils.c*/
+int		increment_index(int i, char *str, char c);
+
+#endif
--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -49,97 +49,53 @@ int	counting(char *str)
 {
 	int		i;
 	int		count;
-	char	c;
 
 	i = 0;
 	count = 0;
 	while (str[i])
 	{
-		if ((str[i] == 34 || str[i] == 39))
-		{
-			c = str[i];
-			i++;
-			while (str[i] && str[i] != c)
-				i++;
-			if (str[i] == '\0')
-				return (0); //TODO manage error case: unclodsed " or '.
-			count++;
-		}
 		if (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
 			count++;
+		if (str[i] == 34 || str[i] == 39)
+		{
+			if (i > 0 && (count == 0 || (str[i + 1] != 32
+				&& !(str[i - 1] >= 9 && str[i - 1] <= 13))))
+				count++;
+			if (i == 0)
+				count++;
+			i = increment_index(i, str, str[i]);
+		}
 		i++;
 	}
+	if (str[i - 1] != 32 && !(str[i - 1] >= 9 && str[i - 1] <= 13)
+			&& str[i - 1] != 34 && str[i - 1] != 39)
+		count++;
 	return (count);
-}
-
-t_node	*gen_list(char *str)
-{
-	int		start;
-	int		i;
-	char	c;
-	t_node	*list;
-	t_node	*tmp;
-
-	start = 0;
-	i = 0;
-	list = malloc(sizeof(t_node));
-	if (!list)
-		return (NULL);
-	tmp = list;
-	while (str[i])
-	{
-		if ((str[i] == 34 || str[i] == 39))
-		{
-			if (start != i)
-			{
-				list = create_node(&str[start], i, list);
-				start = i + 1;
-			}
-			c = str[i];
-			i++;
-			while (str[i] && str[i] != c)
-				i++;
-			list = create_node(&str[start], i - 1, list);
-			start = i + 1;
-		}
-		if (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-		{
-			if (start != i)
-			{
-				list = create_node(&str[start], i - 1, list);
-				start = i + 1;
-			}
-			while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-				i++;
-			list = create_node(&str[start], i - 1, list);
-			start = i + 1;
-		}
-		//printf("[%d] So far so good.\n", i);
-		i++;
-	}
-	list = list->previous;
-	free(list->next);
-	return (list);
 }
 
 char	*parsing(char *cmd)
 {
 	char	*tmp;
+	char	**arg_list;
 	int		node_count;
-	t_node	list;
+	//t_node	list;
 
 	tmp = ft_strdup(cmd);
 	tmp = clean_str(tmp);
 	node_count = counting(tmp); // Subject to change in case of commands with arguments, ex: "cat -e"
-	list = *gen_list(tmp);
-	printf("exited\n");
-	int	i = 0;
-	while (i < node_count)
-	{
-		printf("%s\n", list.value);
-		list = *list.next;
-		i++;
-	}
+	arg_list = malloc(sizeof(char *) * node_count + 1);
+	if (!arg_list)
+		return (NULL);
+	arg_list = gen_arg_list(arg_list, node_count, tmp);
+	//list = *gen_list(tmp);
+	//printf("exited\n");
+	//int	i = 0;
+	//while (i < node_count)
+	//{
+	//	printf("%s\n", list.value);
+	//	list = *list.next;
+	//	i++;
+	//}
 	printf("%d\n", node_count);
 	return (tmp);
 }
