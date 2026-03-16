4ce711c1ed36959e3c3357bbea0654c99884c6c3
Sidney Torck <storck@f4r1s7.clusters.42paris.fr>
2026-03-16 13:59:33 +0100

    Prepared echo for new parsing, will require latching and testing

--- src/Builtin/exec_echo.c
+++ src/Builtin/exec_echo.c
@@ -6,7 +6,7 @@
 /*   By: storck <storck@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/27 12:31:23 by storck            #+#    #+#             */
-/*   Updated: 2026/03/16 10:34:18 by storck           ###   ########.fr       */
+/*   Updated: 2026/03/16 11:31:57 by storck           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,23 +28,31 @@ int	is_option_n(char *str)
 	return (1);
 }
 
+int	check_single_dollar(char *str1, t_quote_type quote1, t_quote_type quote2)
+{
+	if (ft_strcmp(str1, "$"))
+		return (1);
+	else if (quote1 != quote2)
+		return (1);
+	return (0);
+}
+
 int	exec_echo(char **arg, t_quote_type *quote, t_data *data)
 {
 	int		i;
 
 	i = 0;
 	if (!arg || !arg[0])
-	{
-		ft_putchar_fd('\n', STDOUT_FILENO);
-		return (0);
-	}
+		return (ft_putchar_fd('\n', STDOUT_FILENO), 0);
 	while (is_option_n(arg[i]))
 		i++;
 	while (arg[i])
 	{
 		if (do_replace(arg[i], quote[i]))
 			replace_var(&arg[i], data);
-		ft_putstr_fd(arg[i], STDOUT_FILENO);
+		if (!arg[i + 1]
+			|| check_single_dollar(arg[i], quote[i], quote[i + 1]))
+			ft_putstr_fd(arg[i], STDOUT_FILENO);
 		if (arg[i][0] != '\0' && arg[i + 1] != NULL)
 			ft_putchar_fd(' ', STDOUT_FILENO);
 		i++;
